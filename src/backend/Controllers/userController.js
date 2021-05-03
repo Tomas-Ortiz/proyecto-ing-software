4bc4e419b354b8f0a7a636b6a9a5dbba39bf97dd
Tomás Ortiz <devtomas99@gmail.com>
2021-05-03 13:35:03 -0300

    Se devuelve un mensaje de exito o error

--- src/backend/Controllers/userController.js
+++ src/backend/Controllers/userController.js
@@ -2,25 +2,16 @@ const User = require('../Models/User');
 
 const userController = {
   register: async (req, res) => {
-    //let user = req.body;
-    //console.log(user);
+    let message;
     try {
-      const user = new User({
-        type: req.body.type,
-        fullName: req.body.name,
-        NID: req.body.NID,
-        location: req.body.location,
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        contact: req.body.contact,
-      });
+      const user = new User(req.body);
       user.password = await user.encryptPassword(user.password);
       await user.save();
-      res.status(200).send(user);
+      message = 'El registro fue exitoso';
+      res.status(200).send(message);
     } catch (err) {
-      console.log(err.message);
-      res.status(500).send(err.message);
+      message = err.message;
+      res.status(500).send(message);
     }
   },
 };
