cbdd7977a5d46fcdbc07490031e125a23ee25f22
Tomás Ortiz <devtomas99@gmail.com>
2021-05-11 18:29:40 -0300

    Obtener usuario con JWT

--- src/backend/Controllers/userController.js
+++ src/backend/Controllers/userController.js
@@ -1,4 +1,6 @@
+const jwt = require('jsonwebtoken');
 const User = require('../Models/User');
+const config = require('../configs/config');
 
 const userController = {
   register: async (req, res) => {
@@ -18,6 +20,26 @@ const userController = {
         msg: err.message,
       };
       res.status(500).send(result);
+    }
+  },
+
+  getUser: async (req, res) => {
+    let result = {};
+    try {
+      const { token } = req.headers;
+      const userDecoded = await jwt.verify(token, config.key);
+      const user = await User.findOne({ _id: userDecoded.userId }).orFail();
+      result = {
+        success: true,
+        msg: user,
+      };
+      res.status(200).send(result);
+    } catch (err) {
+      result = {
+        success: false,
+        msg: err.message,
+      };
+      res.status(401).send(result);
     }
   },
 };
