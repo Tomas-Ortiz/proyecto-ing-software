ea02ad4809180badecc6397ba1846714a865207a
Augusto <juanaugustolombino@protonmail.ch>
2021-05-19 16:10:14 -0300

    Added Profile Route

--- src/frontend/router/index.js
+++ src/frontend/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue';
 import Signup from '../views/Signup.vue';
+import Profile from '../views/Profile.vue';
 
 const routes = [
   {
@@ -20,6 +21,11 @@ const routes = [
     path: '/signup',
     name: 'Signup',
     component: Signup,
+  },
+  {
+    path: '/profile/:id',
+    name: 'Profile',
+    component: Profile,
   },
 ];
 
