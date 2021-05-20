75fdb9d4618fe311c5fc185e99a094516e6991c1
Facundo <colladofacu540@gmail.com>
2021-05-19 23:28:27 -0300

    login backend

--- src/backend/Controllers/userController.js
+++ src/backend/Controllers/userController.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-empty */
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../Models/User');
 const config = require('../configs/config');
 
@@ -14,6 +16,45 @@ const userController = {
         msg: 'El registro fue exitoso',
       };
       res.status(200).send(result);
+    } catch (err) {
+      result = {
+        success: false,
+        msg: err.message,
+      };
+      res.status(400).send(result);
+    }
+  },
+
+  login: async (req, res) => {
+    let result = {};
+    try {
+      User.findOne({ email: req.body.email }, (err, user) => {
+        if (err) {
+          return res.status(500).json({
+            title: 'server error',
+            error: err,
+          })
+        }
+        if (!user) {
+          return res.status(401).json({
+            title: 'user not found',
+            error: 'Contraseña y/o email incorrecto',
+          })
+        }
+        // incorrect pass
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
+          return res.status(401).json({
+            title: 'login failed',
+            error: 'Contraseña y/o email incorrecto',
+          })
+        }
+        // if all good send id
+        const jToken = jwt.sign({userId: user._id}, 'secretKey');
+        return res.status(200).json({
+          title: 'Login exitoso',
+          token: jToken,
+        })
+      })
     } catch (err) {
       result = {
         success: false,
