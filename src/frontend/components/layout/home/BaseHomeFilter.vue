549d10a0a5168b9830a77e2b043912a5f7123db8
Augusto <juanaugustolombino@protonmail.ch>
2021-05-28 15:55:14 -0300

    Added Pet Cards and Filters
    Created the Pet Card Component and the Filter components fot the Home page

---
+++ src/frontend/components/layout/home/BaseHomeFilter.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="filter-container">
+    <div class="container-title">
+      <h1>FILTROS</h1>
+      <div class="filters">
+        <div class="filter">
+          <h2 class="filter-title">Especie</h2>
+          <p class="filter-type" v-for="item in species" :key="item">{{ item }}</p>
+        </div>
+        <div class="filter">
+          <h2 class="filter-title">Color</h2>
+          <p class="filter-type" v-for="item in species" :key="item">{{ item }}</p>
+        </div>
+        <div class="filter">
+          <h2 class="filter-title">Género</h2>
+          <p class="filter-type" v-for="item in species" :key="item">{{ item }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      species: ['perro', 'gato', 'ave', 'reptil', 'conejo', '...'],
+      colors: ['rojo', 'verde', 'amarillo', 'negro', 'blanco', 'marrón', 'azul', '...'],
+      genders: ['macho', 'hembra', 'desconocido', '...'],
+    }
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.filter-container {
+  @apply min-h-full w-full bg-pink-900;
+}
+.container-title {
+  @apply text-xl md:text-2xl text-gray-200 font-bold text-center tracking-wider;
+}
+.filter {
+  @apply text-sm font-normal text-left my-5 mx-3 md:mx-10 lg:mx-16 xl:mx-20;
+}
+.filter-title {
+  @apply text-lg font-semibold;
+}
+.filter-type {
+  @apply cursor-pointer w-min my-1;
+}
+</style>
--- src/frontend/views/Home.vue
+++ src/frontend/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="home">
     <the-banner></the-banner>
+    <div class="main-home">
+      <the-header class="header"></the-header>
+      <div class="results">
+        <base-home-filter class="filters-container"></base-home-filter>
+        <div class="cards-container">
+          <base-pet-card class="card"></base-pet-card>
+          <base-pet-card class="card"></base-pet-card>
+          <base-pet-card class="card"></base-pet-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import TheBanner from '../components/UI/TheBanner.vue';
+import TheHeader from '../components/UI/TheHeader.vue';
+import BaseHomePetCard from '../components/layout/home/BaseHomePetCard.vue';
+import BaseHomeFilter from '../components/layout/home/BaseHomeFilter.vue';
 
 export default {
   components: {
     'the-banner': TheBanner,
+    'the-header': TheHeader,
+    'base-pet-card': BaseHomePetCard,
+    'base-home-filter': BaseHomeFilter,
   },
   name: 'Home',
 };
@@ -17,6 +34,21 @@ export default {
 
 <style lang="postcss" scoped>
 .home {
-  @apply flex h-screen justify-center items-center;
+  @apply h-screen justify-center items-center bg-gray-200;
+}
+.main-home {
+  @apply h-screen;
+}
+.results {
+  @apply grid grid-cols-12;
+}
+.filters-container {
+  @apply col-span-3 lg:col-span-2;
+}
+.cards-container {
+  @apply col-span-9 p-10 md:grid md:grid-cols-2 lg:grid-cols-3;
+}
+.card {
+  @apply m-auto my-5;
 }
 </style>
