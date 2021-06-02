586d104c0d04adce6e615e67517353e6fa675336
Augusto <juanaugustolombino@protonmail.ch>
2021-06-02 13:01:53 -0300

    Fixed BaseErrorMessage component

--- src/frontend/components/layout/BaseErrorMessage.vue
+++ src/frontend/components/layout/BaseErrorMessage.vue
@@ -1,30 +1,21 @@
 <template>
-  <div :class="['error-message-container', messageVisibility]" role="alert">
-    <strong class="font-bold">{{ errorTitle }}</strong
+  <div class="error-message-container" role="alert">
+    <strong class="error-title">{{ errorTitle }}</strong
     ><br />
     <span class="error-message">{{ errorMsg }}</span>
     <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-      <svg
-        class="fill-current h-6 w-6 text-red-500"
-        role="button"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        @click="
-          messageVisibility =
-            messageVisibility === 'hidden' ? 'hidden' : 'block'
-        "
-      >
-        <title>Close</title>
-        <path
-          d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
-        />
-      </svg>
+      <base-svg-icon class="close-icon" name="close"></base-svg-icon>
     </span>
   </div>
 </template>
 
 <script>
+import BaseSVGIcon from './BaseSVGIcon.vue';
+
 export default {
+  components: {
+    'base-svg-icon': BaseSVGIcon,
+  },
   props: {
     visibility: {
       type: String,
@@ -52,9 +43,19 @@ export default {
 <style lang="postcss" scoped>
 .error-message-container {
   @apply bg-red-100 border border-red-400 text-red-700;
-  @apply w-4/5 mx-auto px-4 py-3 rounded relative;
+  @apply w-4/5 mx-auto px-4 py-3 rounded relative cursor-pointer;
+}
+.error-title,
+.error-message {
+  @apply select-none;
+}
+.error-title {
+  @apply font-bold;
 }
 .error-message {
   @apply block sm:inline;
+}
+.close-icon {
+  @apply w-8 select-none;
 }
 </style>
