23b7a72a309c23387f064a49fe6463b74d8c042d
Augusto <juanaugustolombino@protonmail.ch>
2021-06-02 19:27:35 -0300

    Added Illustration to 404 page

--- src/frontend/views/PageNotFound.vue
+++ src/frontend/views/PageNotFound.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="page-not-found">
     <div>
-      <h1><strong>404 - Página no encontrada :(</strong></h1>
-      <br />
-      <p>Esta página ya no existe o se movió a otra ubicación</p>
+      <h1 class="error-title">404 - Página no encontrada :(</h1>
+      <img
+        src="../assets/illustrations/pet-adoption.svg"
+        alt="Ilustración de una persona observando una página de adopción de mascotas."
+        class="error-image"
+      >
+      <p class="error-text">Esta página ya no existe o se movió a otra ubicación</p>
+      <router-link class="link-home" to="/">Volver a la página principal</router-link>
     </div>
   </div>
 </template>
@@ -12,5 +17,18 @@
 .page-not-found {
   @apply flex h-screen justify-center items-center;
   @apply text-center text-red-500;
+}
+.error-title {
+  @apply font-bold;
+}
+.error-text,
+.error-title {
+  @apply my-5;
+}
+.error-image {
+  @apply max-w-xl;
+}
+.link-home {
+  @apply text-gray-200 bg-pink-900 px-3 py-2 rounded-md;
 }
 </style>
